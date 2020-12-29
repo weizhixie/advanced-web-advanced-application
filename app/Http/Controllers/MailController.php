465a21014db6e01d737229a1d29003d79cf7c74f
weizhixie <u1757949@unimail.hud.ac.uk>
2020-12-29 19:35:05 +0000

    send welcome email after user registration

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\SnackShopEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    public function welcomeMessage($userEmail)
+    {
+        Mail::to($userEmail)->send(new SnackShopEmail());
+    }
+}
---
+++ resources/views/email/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section ('sidebar')
+    <div class="flex justify-between m-3">
+        <div class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            <h1>Hi</h1>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <h1>Welcome to Snack Shop</h1>
+    <h2>This is test email</h2>
+@endsection
+
